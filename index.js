11d3ac1e89470e5442aa4bf87f3941bb99843bf9
Ajackwere <austinejackwere@gmail.com>
2023-06-22 23:42:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, string){
+    const searchLowerCase = string.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().includes(searchLowerCase));
+}
+function fuzzyMatch(drivers, string){
+    const searchLowerCase = string.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(searchLowerCase));
+}
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name);
+}
